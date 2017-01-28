ff84d10c9c6c51f7c64535b6ff5fc16e8827b60c
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-01-28 17:11:00 -0600

    debugging

--- src/actions/homepage_actions.js
+++ src/actions/homepage_actions.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { FETCH_PROFILE } from '../constants/homepage_constants';
-import { ROOT_URL } from '../constants/application_constants';
-
 
 export function fetchProfile () {
-    const URL = `${ROOT_URL}/data`;
+    const URL = `/data`;
     const request = axios.get(URL);
 
     return { type: FETCH_PROFILE, payload: request }
